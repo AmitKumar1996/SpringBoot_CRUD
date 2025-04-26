0daf819f9bf87c3c8e01a40434910074d48f9fbe
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-04-27 00:55:53 +0530

    Delete Controller

--- src/main/java/dev/Amit/EmployeeAPICRUD/EmployeeController/EmployeeController.java
+++ src/main/java/dev/Amit/EmployeeAPICRUD/EmployeeController/EmployeeController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -59,5 +61,60 @@ public class EmployeeController {
 		
 		
 	}
+	
+	@PutMapping("/employee/{empid}")
+	public String updateEmployeeById(@PathVariable long empid, @RequestBody Employee employee) {
+		// Interview Question 
+	Optional<Employee> emp=	employeeRepository.findById(empid);
+	if(emp.isPresent()) {
+		Employee existEmp=emp.get();
+		existEmp.setEmpAge(employee.getEmpAge());
+		existEmp.setEmpCity(employee.getEmpCity());
+//		existEmp.setEmpid(employee.getEmpid());
+		existEmp.setEmpName(employee.getEmpName());
+		existEmp.setEmpSalary(employee.getEmpSalary());
+		employeeRepository.save(existEmp);
+		return "Employee Details against Id "+ empid+" updated";
+		
+		
+	}
+	else {
+		return "Employee Details does not exist for "+ empid;
+		}
+	}
+	
+	
+	@DeleteMapping("/employee/{empId}")
+	public String deleteById(@PathVariable long empId ) {
+	Optional<Employee> deleteEmp=	employeeRepository.findById(empId);
+	if(deleteEmp.isPresent()) {
+		Employee delete = deleteEmp.get();
+	      employeeRepository.delete(delete);
+	      return "Employee is deleted "+empId;
+	}
+	else {
+		return empId+" is not found in DB";
+		
+	}
+		
+	}
+	
+	// my way
+//	@DeleteMapping("/employee")
+//	public String deleteAllEmp() {
+//		
+//		employeeRepository.deleteAll();
+//		
+//		
+//		return "All Employee is deleted";
+//		
+//	}
+	
+	@DeleteMapping("/employee")
+	public ResponseEntity<String> deleteAllEmp() {
+	    employeeRepository.deleteAll();
+	    return ResponseEntity.ok("All Employees have been deleted");
+	}
+
 
 }
