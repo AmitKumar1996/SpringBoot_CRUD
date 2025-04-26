30883bc51ded0b6515b6d89091d0551bba143f96
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-04-26 23:30:14 +0530

    find by ID contreoller

--- src/main/java/dev/Amit/EmployeeAPICRUD/EmployeeController/EmployeeController.java
+++ src/main/java/dev/Amit/EmployeeAPICRUD/EmployeeController/EmployeeController.java
@@ -2,11 +2,14 @@ package dev.Amit.EmployeeAPICRUD.EmployeeController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,7 +45,17 @@ public class EmployeeController {
 	} 
 	
 	@GetMapping("/employee/{empid}")
-	public ResponceEntity<Employee> getEmployeeById(@pathVariable long empid){
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable long empid){
+		
+		// interview question 
+		Optional<Employee> emp= employeeRepository.findById(empid);
+		
+		if(emp.isPresent()) {
+			return new ResponseEntity<Employee>(emp.get(), HttpStatus.FOUND);
+		}
+		else {
+			return new ResponseEntity<Employee>(HttpStatus.NOT_FOUND);
+		}
 		
 		
 	}
